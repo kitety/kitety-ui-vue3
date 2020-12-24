188c0dbc0e318938d81aaa71d8f2896fe4cd9c68
kitety <1048444579@qq.com>
2020-12-24 14:13:09 +0800

    feat: 目录结构完善

---
+++ story/app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+};
+</script>
+
+<style lang="scss"></style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
+const path = require("path");
+const resolve = (filePath) => path.join(__dirname, "./", filePath);
+
 module.exports = {
   outputDir: "docs",
-  publicPath:  "./",
+  publicPath: "./",
+  devServer: { port: "3366" },
+  pages: {
+    index: {
+      entry: resolve("story/main.ts"),
+      template: "public/index.html",
+      filename: "index.html",
+      title: "kitety-ui-vue3",
+    },
+  },
+  chainWebpack: (config) => {
+    config.plugins.delete("prefetch-index").delete("preload-index");
+    config.resolve.alias.set("story", resolve("story"));
+  },
 };
