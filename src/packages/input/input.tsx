ef38d3396cc547c3136d26de5ef57bd005a7e94f
kitety <1048444579@qq.com>
2020-12-25 17:56:57 +0800

    feat: input添加focus和clear

--- src/packages/input/input.tsx
+++ src/packages/input/input.tsx
@@ -11,8 +11,18 @@ export default defineComponent({
     const modelValue = ref('')
     const inputRef = ref(null as null | HTMLInputElement)
     const classes = computed(() => ['pl-input', `pl-input-status-${props.status}`])
+    const methods = {
+      focus: () => {
+        inputRef.value!.focus()
+      },
+      clear: () => {
+        modelValue.value = ''
+      }
+    }
     return () => <div class={classes.value}>
       <input class="pl-input-inner" type="text" v-model={modelValue.value} ref={inputRef} />
+      <button onClick={methods.clear}>Clear</button>
+      <button onClick={methods.focus}>Focus</button>
     </div>
   }
 })
